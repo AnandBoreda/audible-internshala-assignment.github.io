9c0c145229287fe5b1427c8ad79fa45ce29e1449
anandboreda <zz.anand2016@gmail.com>
2020-12-05 20:36:50 +0530

    code refractored

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 const audio = document.getElementById("audio");
 
+const play = document.getElementById("play");
+const pause = document.getElementById("pause");
+const stop = document.getElementById("stop");
+
+//check if lcoalstorage contains previous bookmarks and update the source with the same 
 function setBookmark(){
     var pos = localStorage.getItem("seek");
-    
     console.log("bookmarked seek position is "+pos);
-
     audio.currentTime = pos;
 }
-  
+
+// set bookmark to localstorage
 function bmark(){
 
     bookmark = audio.currentTime;
@@ -15,21 +19,19 @@ function bmark(){
     console.log(bookmark);
 }
 
-const play = document.getElementById("play");
-const pause = document.getElementById("pause");
-const stop = document.getElementById("stop");
-
+// play audio 
 function playAudio(){
     play.style.display = "none";
     pause.style.display = "block";
     audio.play();
 }
+// pause audio
 function pauseAudio(){
     play.style.display = "block";
     pause.style.display = "none";
     audio.pause();
 }
-
+// stop audio
 function stopAudio(){
     audio.pause();
     audio.currentTime = 0;
